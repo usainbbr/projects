e7d62a24d52fcaa468b5ed2e772726393534f548
usainbbr <83027533+usainbbr@users.noreply.github.com>
2021-04-22 17:30:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="assets/stylesheets/index.css"/>
+    <link rel="icon" type="image/x-icon" href="assets/images/logo.png"/>
+    <title>Homepage - INEC APS</title>
+</head>
+<body>
+<header id="header">
+    <div class="logo"><img src="assets/images/logo.png"/></div>
+    <h1>Independent National Electoral Commission</h1>
+    <h4>Automated Payroll System (APS)</h4></header>
+<hr/>
+<nav></nav>
+<div class="gateway">
+    <a href="adminstrative/index.php">
+        <img class="image" src="assets/images/people.png" height="400"/></a>
+    <hr/>
+    <h6>Administrative Portal</h6>
+    finance and supply department, financial aspect,record
+</div>
+<div class="gateway">
+    <a href="Home">
+        <img class="image" src="assets/images/workers1.png" height="400"/></a>
+    <hr/>
+    <h6>Employee Portal</h6>
+    staff records pension,salary, loan, statements and complaint
+</div>
+<footer id="footer">
+    &copy;2015 INEC
+</footer>
+</body>
+</html>
